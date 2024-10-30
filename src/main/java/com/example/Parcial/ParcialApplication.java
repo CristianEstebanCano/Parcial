7711b994bf799ec38e780e7198d4c76f1c120251
CristianEstebanCano <canox09@gmail.com>
2024-10-29 22:04:06 -0500

    Arreglo Nombre

--- src/main/java/com/example/Parcial/ParcialApplication.java
+++ src/main/java/com/example/Parcial/ParcialApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProyectoAlmacenApplication {
+public class ParcialApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProyectoAlmacenApplication.class, args);
+		SpringApplication.run(ParcialApplication.class, args);
 	}
 
 }
